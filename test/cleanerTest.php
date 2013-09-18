fd8a572ecd42a2c2495af8d7b8101755de93e764
Eustaquio Rangel <taq@eustaquiorangel.com>
2013-09-18 13:29:28 -0300

    Must use the sent path on cleaner.  Mudanças a serem submetidas:

--- test/cleanerTest.php
+++ test/cleanerTest.php
@@ -3,13 +3,13 @@ include_once "../cleaner.php";
 
 class CleanerTest extends PHPUnit_Framework_TestCase {
    protected static $cleaner = null;
-   protected static $dir     = "/tmp/delayedmailtest/";
+   protected static $dir     = "/tmp/delayedmailtest/sent/";
 
    public function setUp() {
       self::$cleaner = new DelayedMail\Cleaner(self::$dir,5);
    }
 
    public function testToString() {
-      $this->assertEquals("cleaning files older than 5 minutes on /tmp/delayedmailtest/",self::$cleaner."");
+      $this->assertEquals("cleaning files older than 5 minutes on /tmp/delayedmailtest/sent/",self::$cleaner."");
    }
 }
